6826aa87a0929a21f3274e95e21f45443b5a0598
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-01-25 13:36:07 -0300

    criado api

---
+++ api-hub-inovacao/src/main/java/br/com/apihubinovacao/ApiHubInovacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.apihubinovacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHubInovacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiHubInovacaoApplication.class, args);
+    }
+
+}
---
+++ api-hub-inovacao/src/test/java/br/com/apihubinovacao/ApiHubInovacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apihubinovacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHubInovacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
