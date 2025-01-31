1c692d4df4da619d6f0293e960b88f760fbff49f
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-01-31 00:31:33 -0300

    ajustado bug onde naão chamava projetos aprovados.

---
+++ api-hub-inovacao/src/main/java/br/com/apihubinovacao/domain/dtos/projects/AcademicProjectResponseProfessorApprovedDTO.java
@@ -0,0 +1,21 @@
+package br.com.apihubinovacao.domain.dtos.projects;
+
+import br.com.apihubinovacao.domain.dtos.coauthor.CoauthorDTO;
+import br.com.apihubinovacao.domain.enums.TypeAP;
+
+import java.util.List;
+
+public record AcademicProjectResponseProfessorApprovedDTO(
+        Long id,
+        String title,
+        String description,
+        String urlPhoto,
+        String pdfLink,
+        String siteLink,
+        TypeAP typeAP,
+        String currentUserEmail,
+        String creationDate,
+        Long studentId,
+        Long professorId,
+        List<CoauthorDTO> coauthors
+) {}
