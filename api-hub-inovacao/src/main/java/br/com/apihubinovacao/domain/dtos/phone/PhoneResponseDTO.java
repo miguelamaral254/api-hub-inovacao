7eb955dee4a4815ae041f14f46f0812da53d7cfa
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-03-22 23:52:55 -0300

    ajustado develop

--- api-hub-inovacao/src/main/java/br/com/apihubinovacao/domain/dtos/phone/PhoneResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package br.com.apihubinovacao.domain.dtos.phone;
-
-
-public record PhoneResponseDTO(Long id, String number) {}
--- api-hub-inovacao/src/main/java/br/com/apihubinovacao/domain/usecases/startup/get/ListAllStartupsForManagerUseCase.java
+++
@@ -1,4 +0,0 @@
-package br.com.apihubinovacao.domain.usecases.startup.get;
-
-public class ListAllStartupsForManagerUseCase {
-}
